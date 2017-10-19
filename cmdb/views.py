22220151d649d0fb6d127fefe59522747b9d3641
ga0404 <ga0404@21cn.com>
2017-10-19 18:48:50 +0800

    修改为Git Test3

--- cmdb/views.py
+++ cmdb/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    #return HttpResponse("Hello World!")
+    return HttpResponse("Git Test")
+    return HttpResponse("Git Test3")
